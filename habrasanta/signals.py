a2e4542c0d8ae9a1bbc5f1094b7ebbf773f9d234
kafeman <kafemanw@gmail.com>
2023-12-19 22:20:49 +0100

    Warn admins per e-mail when they sign in with their Habr account

--- habrasanta/signals.py
+++ habrasanta/signals.py
@@ -1,9 +1,22 @@
+from django.conf import settings
+
+from habrasanta.celery import send_email
 from habrasanta.models import Event
 
 
 def log_user_login(sender, user, request, **kwargs):
     if not user:
         return
+    if user.is_staff and not settings.DEBUG:
+        send_email.delay(
+            user.id,
+            "произведен вход в ваш аккаунт",
+            "Приветствуем, {}!\n\n".format(user.login) +
+            "Так как у вас имеется доступ в админку Хабра-АДМ, то мы вынуждены проинформировать вас о новом входе под вашим аккаунтом:\n\n" +
+            "IP-адрес: {}\n".format(request.META["REMOTE_ADDR"]) +
+            "User-Agent: {}\n\n".format(request.META.get("HTTP_USER_AGENT", "неизвестен")) +
+            "Если это были не вы, просьба немедленно сообщить kafeman'у."
+        )
     Event.objects.create(
         typ=Event.LOGGED_IN,
         sub=user,
