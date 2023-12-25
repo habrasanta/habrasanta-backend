95541c38f367ab4a4e3c8765efca61f374dbc977
kafeman <kafemanw@gmail.com>
2023-12-25 11:00:47 +0100

    Log errors when requesting Habr API

--- habrasanta/utils.py
+++ habrasanta/utils.py
@@ -1,8 +1,12 @@
+import logging
 import requests
 import time
 
 from django.conf import settings
 from django.core.cache import cache
+
+
+logger = logging.getLogger(__name__)
 
 
 session = requests.Session()
@@ -19,12 +23,16 @@ def fetch_habr_profile(username):
             "apikey": settings.HABR_APIKEY,
         }, timeout=(0.5, 1.0))
         if response.status_code != 200:
+            logger.warning("Request to {} failed: got status code {}".format(response.url, response.status_code))
+            logger.warning(response.text)
             return None
         card = response.json()
         response = session.get("https://habr.com/api/v2/users/{}/whois".format(username), headers={
             "apikey": settings.HABR_APIKEY,
         }, timeout=(0.5, 1.0))
         if response.status_code != 200:
+            logger.warning("Request to {} failed: got status code {}".format(response.url, response.status_code))
+            logger.warning(response.text)
             return None
         whois = response.json()
         end = time.time()
