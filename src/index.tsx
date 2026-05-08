ac4070bc0430408706bc6bcd20860848e3b432de
kafeman <kafemanw@gmail.com>
2026-05-08 23:45:19 +0200

    Try a better way to integrate Django+Preact

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV !== "production") {
-  require("preact/debug");
+if (import.meta.env.DEV) {
+  import("preact/debug");
 }
 
 import { h, render } from "preact";
@@ -7,6 +7,12 @@ import { h, render } from "preact";
 import "./index.css"; // include before the App component
 
 import App from "./App";
+
+// Used by the frontend.html frontend template.
+import "./images/apple-touch-icon.png?no-inline";
+import "./images/favicon-16x16.png?no-inline";
+import "./images/favicon-32x32.png?no-inline";
+import "./images/social.jpg?no-inline";
 
 document.addEventListener("DOMContentLoaded", (event) => {
   render(<App />, document.getElementById("root")!);
